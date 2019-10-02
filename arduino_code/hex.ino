1446866e7ed8ab199fd9030b1701a5d56e0f129c
Artiom <48057470+ArtiomBoo@users.noreply.github.com>
2019-10-02 21:13:26 +0200

    Add files via upload

--- arduino_code/hex.ino
+++ arduino_code/hex.ino
@@ -4,44 +4,38 @@
 #include <WidgetRTC.h>
 #include <TimeLib.h>
 
-#define BLYNK_PRINT Serial
+//---------------Settings-------------------------------------------------------
+#define LEDS_IN_BOX 24                            /* Number of LEDs in each box/leaf */
+#define NUM_BOXES 8                               /*The number of boxes */
+#define PIN 6                                     /*The pin the LED is connected to */
+#define BRIGHTNESS 50                             /* Default brightness */
+char ssid[] = "ZyXELE5E9F4";                      /* wifi ssid name */
+char pass[] = "B959124795FF";                     /* wifi pass */
+char auth[] = "0034273f9bbe4e91a4072440f7a932ce"; /* blynk auth code */
+//------------------------------------------------------------------------------
 
-#define LEDS_IN_BOX 24 /* Number of LEDs in each box/leaf */
-#define NUM_BOXES 8 /*The number of boxes */
-#define TOTAL_LEDS  LEDS_IN_BOX * NUM_BOXES
-#define PIN 6 /*The pin the LED is connected to */
+#define BLYNK_PRINT Serial
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-#define BRIGHTNESS 50
-
-CRGB leds[TOTAL_LEDS];
-
-char ssid[] = "ZyXELE5E9F4";
-char pass[] = "B959124795FF";
-char auth[] = "0034273f9bbe4e91a4072440f7a932ce";
+#define TOTAL_LEDS  LEDS_IN_BOX * NUM_BOXES
 
 //Global variables
-int wifiStatus;
-bool leds_enable = true;
-bool blynk_connection;
-bool change;
-uint8_t red, green, blue, led_speed;
-uint8_t effect = 2; 
-uint8_t red2, green2, blue2;
-uint8_t red3, green3, blue3;
+CRGB leds[TOTAL_LEDS];
+bool leds_enable = true;                //leds on/off
+uint8_t effect = 2;                     // default effect
+uint8_t transition = 2;                 // default transition effect
+uint8_t red, green, blue, led_speed;    //main color
+uint8_t red2, green2, blue2;            //secondary color
 bool autoLED = false;
-bool autoTime = false;
 bool syncOnChange = false;
 int delay_time = 200;
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 int leaf = 1;
-
 WidgetLCD lcd(V0);
 
 void setup() {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
-  FastLED.addLeds<LED_TYPE, PIN, COLOR_ORDER>(leds, TOTAL_LEDS ).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, PIN, COLOR_ORDER>(leds, TOTAL_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);// global brightness
 }
 
@@ -50,48 +44,47 @@ void loop() {
     Blynk.syncAll();
     syncOnChange = false;
   }
-  change = false;
   Blynk.run();
   if (leds_enable) {
     switch (effect) {
       case 0:
-        break;
+      break;
       case 1:
-        leds_off();
-        break;
+      leds_off();
+      break;
       case 2:
-        one_color();
-        break;
+      one_color();
+      break;
       case 3:
-        two_colors(delay_time);
-        break;
+      two_colors(delay_time);
+      break;
       case 4:
-        
-        break;
+      two_colors_gradient(red, green, blue, red2, green2, blue2, delay_time);
+      two_colors_gradient(red2, green2, blue2, red, green, blue, delay_time);
+      break;
       case 5:
-
-        break;
+      two_colors_gradient2(red, green, blue, red2, green2, blue2, delay_time);
+      two_colors_gradient2(red2, green2, blue2, red, green, blue, delay_time);
+      break;
       case 6:
-        random_colors(delay_time);
-        break;
+      random_colors(delay_time);
+      break;
       case 7:
-        each_leaf(delay_time);
-        break;   
+      each_leaf(delay_time);
+      break;
     }
   }
   else {
     leds_off();
   }
 }
-//---------------- Blynk inputs ------------------------
+
+//---------------- Blynk inputs ------------------------------------------------
 BLYNK_WRITE(V1) { // on/off
-  Serial.println(param.asInt());
   leds_enable = param.asInt();
-  Serial.println(leds_enable);
-  change = true;
   if (leds_enable) {
-    Blynk.virtualWrite(8, 0);
-    autoLED = false;
+    Blynk.virtualWrite(8, 0); //remove?
+    autoLED = false; //remove?
   }
 }
 
@@ -99,21 +92,12 @@ BLYNK_WRITE(V3) { // main color
   red = param[0].asInt();
   green = param[1].asInt();
   blue = param[2].asInt();
-  change = true;
 }
 
 BLYNK_WRITE(V7) { // secondary color
   red2 = param[0].asInt();
   green2 = param[1].asInt();
   blue2 = param[2].asInt();
-  change = true;
-}
-
-BLYNK_WRITE(V4) { // secondary color
-  red3 = param[0].asInt();
-  green3 = param[1].asInt();
-  blue3 = param[2].asInt();
-  change = true;
 }
 
 BLYNK_WRITE(V2) { // brightness
@@ -128,15 +112,17 @@ BLYNK_WRITE(V5) { // leaf
   leaf = param.asInt();
 }
 
-BLYNK_WRITE(V9) { // menu
+BLYNK_WRITE(V9) { // effects menu
   Serial.println(param.asInt());
   effect = param.asInt();
-  change = true;
 }
 
+BLYNK_WRITE(V4) { // transitions menu
+  Serial.println(param.asInt());
+  transition = param.asInt();
+}
 
-
-//---------------- Effects ------------------------
+//---------------- Effects -----------------------------------------------------
 void leds_off() {
   for (int i = 0; i < TOTAL_LEDS; i++) {
     leds[i] = CRGB::Black;
@@ -151,30 +137,397 @@ void one_color() {
   FastLED.show();
 }
 
-
 void rainbow(int wait) {
 
 }
 
 void two_colors(int wait) {
-  box(random(0,9), red, green, blue); // main color
-  delay(wait);
-  box(random(0,9), red2, green2, blue2); // secondary color
-  delay(wait);
+  box(random(0, 9), red, green, blue, transition, wait); // main color
+  box(random(0, 9), red2, green2, blue2, transition, wait); // secondary color
 }
 
-void two_colors_gradient(int wait) {
+void two_colors_gradient2(int r, int g, int b, int r2, int g2, int b2, int delay_time){
+  transition_left_to_right(48, 71, r, g, b, r2, g2, b2, delay_time);
+  transition_left_to_right_2_boxes(24, 47, 72, 95, r, g, b, r2, g2, b2, delay_time);
+  transition_left_to_right_2_boxes(0, 23, 96, 119, r, g, b, r2, g2, b2, delay_time);
+  transition_left_to_right_2_boxes(168, 191, 120, 143, r, g, b, r2, g2, b2, delay_time);
+  transition_left_to_right(144, 167, r, g, b, r2, g2, b2, delay_time);
+}
 
+void two_colors_gradient(int r, int g, int b, int r2, int g2, int b2, int delay_time){
+  int count = 0;
+  int value1 = 164;
+  int value2 = 163;
+
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+
+    if(value1 > 167){
+      value1 = 144;
+    }
+    count++;
+  }while(count < 10);
+
+  printf("-----------------\n");
+  count = 0;
+  int value3 = 188;
+  int value4 = 187;
+  int value5 = 140;
+  int value6 = 139;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+    value3++, value4--;
+    value5++, value6--;
+    count++;
+  }while(count < 2);
+
+  printf("-----------------\n");
+  count = 0;
+  do{
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    FastLED.show();
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    delay(delay_time);
+    value3++, value4--;
+    value5++, value6--;
+
+    if(value3 > 191){
+      value3 = 168;
+    }
+    if(value5 > 143){
+      value5 = 120;
+    }
+    count++;
+  }while(count < 8);
+
+  printf("-----------------\n");
+  count = 0;
+  value1 = 20;
+  value2 = 19;
+  int value7 = 116;
+  int value8 = 115;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    leds[value7].setRGB(r, g, b);
+    leds[value8].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    leds[value7].setRGB(r2, g2, b2);
+    leds[value8].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+    value3++, value4--;
+    value5++, value6--;
+    value7++, value8--;
+    count++;
+  }while(count < 2);
+
+  printf("-----------------\n");
+  count = 0;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value7].setRGB(r, g, b);
+    leds[value8].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    leds[value7].setRGB(r2, g2, b2);
+    leds[value8].setRGB(r2, g2, b2);
+    delay(delay_time);
+    value1++, value2--;
+    value7++, value8--;
+
+    if(value1 > 23){
+      value1 = 0;
+    }
+    if(value7 > 119){
+      value7 = 96;
+    }
+    count++;
+  }while(count < 8);
+
+  printf("-----------------\n");
+  count = 0;
+  value3 = 44;
+  value4 = 43;
+  value5 = 92;
+  value6 = 91;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    leds[value7].setRGB(r, g, b);
+    leds[value8].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    leds[value7].setRGB(r2, g2, b2);
+    leds[value8].setRGB(r2, g2, b2);
+    delay(delay_time);
+    value1++, value2--;
+    value3++, value4--;
+    value5++, value6--;
+    value7++, value8--;
+    count++;
+  }while(count < 2);
+
+  printf("-----------------\n");
+  count = 0;
+  do{
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    FastLED.show();
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    delay(delay_time);
+    value3++, value4--;
+    value5++, value6--;
+
+    if(value3 > 47){
+      value3 = 24;
+    }
+    if(value5 > 95){
+      value5 = 72;
+    }
+    count++;
+  }while(count < 8);
+
+  printf("-----------------\n");
+  count = 0;
+  value1 = 68;
+  value2 = 67;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    leds[value3].setRGB(r2, g2, b2);
+    leds[value4].setRGB(r2, g2, b2);
+    leds[value5].setRGB(r2, g2, b2);
+    leds[value6].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+    value3++, value4--;
+    value5++, value6--;
+    count++;
+  }while(count < 2);
+
+  printf("-----------------\n");
+  count = 0;
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+
+    if(value1 > 71){
+      value1 = 48;
+    }
+    count++;
+  }while(count < 10);
 }
 
 void random_colors(int wait) {
-  box(random(0,9), random(255), random(255), random(255));
-  delay(wait);
+  box(random(0, 9), random(255), random(255), random(255), transition, wait);
 }
 
 void each_leaf(int wait) {
-  box(leaf, red3, green3, blue3);
-  delay(wait);
+  box(leaf, red, green, blue, transition, wait);
+}
+
+//********* Transitions ***********************************************
+
+void transition_simple(int i, int j, int r, int g, int b, int delay_time){
+  for (i; i <= j ; ++i) {
+    leds[i].setRGB(r, g, b);
+  }
+  FastLED.show();
+  delay(delay_time);
+}
+
+void transition_circle(int i, int j, int r, int g, int b, int delay_time){
+  for (i; i <= j ; ++i) {
+    leds[i].setRGB(r, g, b);
+    FastLED.show();
+    delay(delay_time);
+  }
+}
+
+void transition_2_sides(int i, int j, int r, int g, int b, int delay_time){
+  int count = 1;
+
+  int value1 = i + 7;
+  int value2 = i + 8;
+  int value3 = i + 19;
+  int value4 = i + 20;
+
+  leds[value1].setRGB(r, g, b);
+  leds[value2].setRGB(r, g, b);
+  leds[value3].setRGB(r, g, b);
+  leds[value4].setRGB(r, g, b);
+  FastLED.show();
+  delay(delay_time);
+
+  do{
+    value1--, value2++, value3--, value4++;
+
+    if(value4 > j){
+      value4 = i;
+    }
+
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    FastLED.show();
+    delay(delay_time);
+    count++;
+  }while(count < 6);
+}
+
+void transition_6_corners(int i, int j, int r, int g, int b, int delay_time){
+  int count = 1;
+
+  int value1 = i;
+  int value2 = i + 4;
+  int value3 = i + 8;
+  int value4 = i + 12;
+  int value5 = i + 16;
+  int value6 = i + 20;
+
+  leds[value1].setRGB(r, g, b);
+  leds[value2].setRGB(r, g, b);
+  leds[value3].setRGB(r, g, b);
+  leds[value4].setRGB(r, g, b);
+  leds[value5].setRGB(r, g, b);
+  leds[value6].setRGB(r, g, b);
+  FastLED.show();
+  delay(delay_time);
+
+  do{
+    value1++, value2++, value3++, value4++, value5++, value6++;
+
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    leds[value3].setRGB(r, g, b);
+    leds[value4].setRGB(r, g, b);
+    leds[value5].setRGB(r, g, b);
+    leds[value6].setRGB(r, g, b);
+    FastLED.show();
+    delay(delay_time);
+    count++;
+  }while(count < 4);
+}
+
+void transition_left_to_right(int i, int j, int r, int g, int b, int r2, int g2, int b2, int delay_time){
+  int count = 0;
+  int value1 = i+20;
+  int value2 = i+19;
+
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+
+    if(value1 > j){
+      value1 = i;
+    }
+    count++;
+  }while(count < 12);
+}
+
+void transition_left_to_right_2_boxes(int i, int j, int i2, int j2, int r, int g, int b, int r2, int g2, int b2, int delay_time){
+  int count = 0;
+  int value1 = i+20;
+  int value2 = i+19;
+  int value3 = i2+20;
+  int value4 = i2+19;
+
+  do{
+    leds[value1].setRGB(r, g, b);
+    leds[value2].setRGB(r, g, b);
+    FastLED.show();
+    leds[value1].setRGB(r2, g2, b2);
+    leds[value2].setRGB(r2, g2, b2);
+    delay(delay_time);
+
+    value1++, value2--;
+    value3++, value4--;
+
+    if(value1 > j){
+      value1 = i;
+    }
+    if(value3 > j2){
+      value3 = i2;
+    }
+    count++;
+  }while(count < 12);
 }
 
 //********* Help method ***********************************************
@@ -198,45 +551,60 @@ CRGB wheel(int WheelPos, int dim) {
   return color;
 }
 
-void box(int nr, int r, int g, int b){
+void box(int nr, int r, int g, int b, uint8_t transition, int delay_time) {
   int i = 0;
   int j = 0;
-  
-  if(nr == 1){
+
+  switch (nr) {
+    case 1:
     i = 0;
-    j = 24;
+    j = 23;
+    break;
+    case 2:
+    i = 24;
+    j = 47;
+    break;
+    case 3:
+    i = 48;
+    j = 71;
+    break;
+    case 4:
+    i = 72;
+    j = 95;
+    break;
+    case 5:
+    i = 96;
+    j = 119;
+    break;
+    case 6:
+    i = 120;
+    j = 143;
+    break;
+    case 7:
+    i = 144;
+    j = 167;
+    break;
+    case 8:
+    i = 168;
+    j = 191;
+    break;
   }
-  else if(nr == 2){
-    i = 25;
-    j = 48;
+
+  switch (transition) {
+    case 0:
+    break;
+    case 1:
+    transition_simple(i, j, r, g, b, delay_time);
+    break;
+    case 2:
+    transition_circle(i, j, r, g, b, delay_time);
+    break;
+    case 3:
+    transition_2_sides(i, j, r, g, b, delay_time);
+    break;
+    case 4:
+    transition_6_corners(i, j, r, g, b, delay_time);
+    break;
   }
-  else if(nr == 3){
-    i = 49;
-    j = 72;
-  } 
-  else if(nr == 4){
-    i = 73;
-    j = 96;
-  } 
-  else if(nr == 5){
-    i = 97;
-    j = 120;
-  } 
-  else if(nr == 6){
-    i = 121;
-    j = 144;
-  } 
-  else if(nr == 7){
-    i = 145;
-    j = 168;
-  } 
-  else if(nr == 8){
-    i = 169;
-    j = 192;
-  } 
- 
-  for (i; i < j ; ++i) { 
-    leds[i].setRGB(r, g, b);
-  }
-  FastLED.show();
+
 }
